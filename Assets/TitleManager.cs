12f68569bd6348e8d2a714307c485692f538860c
202004g07 <66996129+202004g07@users.noreply.github.com>
2020-09-14 20:35:35 +0900

    fin

--- Assets/TitleManager.cs
+++ Assets/TitleManager.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class TitleManager : MonoBehaviour
 {
+    public float Power = 0;
+    public GameObject Apple;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Apple.GetComponent<Rigidbody>().AddForce(new Vector3(-Power, 0, 0));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
